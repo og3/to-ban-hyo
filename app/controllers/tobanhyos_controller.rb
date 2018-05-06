e3e082b272e03e48408fcd7cc0001c715fef81d7
noriyuki togashi <>
2018-05-06 09:32:56 +0900

    create_send_line_msg_function

---
+++ app/controllers/tobanhyos_controller.rb
@@ -0,0 +1,37 @@
+class TobanhyosController < ApplicationController
+  require 'line/bot'
+  protect_from_forgery :except => [:send_line_msg]
+
+  def rotate
+    tobanhyo = Tobanhyo.where(fixed: nil)
+    new_roles = tobanhyo.map{ |hyo| hyo.role_id }.rotate!
+    tobanhyo.zip(new_roles).each do |hyo, new_role_id|
+      hyo.role_id = new_role_id
+      hyo.save!
+    end
+  end
+
+  def create_line_msg
+    tobanhyo = Tobanhyo.all
+    msg = "今週の掃除当番です！\n"
+    msg << "[#{Time.now.strftime('%Y/%m/%d')} 〜 #{Time.now.since(7.days).strftime('%Y/%m/%d')}]\n"
+    tobanhyo.each do |hyo|
+      msg << "#{hyo.room.name}: #{hyo.role.name}\n"
+    end
+    msg
+  end
+
+  def send_line_msg
+    rotate
+    message = {
+      type: 'text',
+      text: create_line_msg
+    }
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_ACCESS_TOKEN"]
+    }
+    response = client.push_message("C0eecebeefc99fb949035edb04bc95023", message)
+    p response
+  end
+end
--- app/models/tobanhyo.rb
+++ app/models/tobanhyo.rb
@@ -1,22 +1,4 @@
 class Tobanhyo < ApplicationRecord
   belongs_to :role
   belongs_to :room
-
-  def rotate
-    tobanhyo = Tobanhyo.where(fixed: 0)
-    new_roles = tobanhyo.map{ |hyo| hyo.role_id }.rotate!
-    tobanhyo.zip(new_roles).each do |hyo, role|
-      hyo.role_id = role
-      hyo.save!
-    end
-  end
-
-  def create_line_msg
-    tobanhyo = Tobanhyo.all
-    puts "今週の掃除当番です！"
-    puts "[#{Time.now.strftime('%Y/%m/%d')} 〜 #{Time.now.since(7.days).strftime('%Y/%m/%d')}]"
-    tobanhyo.each do |hyo|
-      puts "#{hyo.room.name}: #{hyo.role.name}"
-    end
-  end
 end
