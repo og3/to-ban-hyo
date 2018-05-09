b9c74d8e38d65ac6378e9bd47aea670b84058971
noriyuki togashi <>
2018-05-09 21:52:53 +0900

    自動投稿機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # linedevで設定しているwebhookURLと同じにすると、グループユーザーのコメント等に反応してしまうため、URLを変更した
+  post '/webhook2' => 'tobanhyos#send_line_msg'
 end
