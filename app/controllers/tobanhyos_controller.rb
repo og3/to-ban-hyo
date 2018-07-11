b45bba4dc50954c28eb6b83c417208ecf6bcd9ab
noriyuki togashi <>
2018-07-12 06:45:13 +0900

    一時コミット

--- app/controllers/tobanhyos_controller.rb
+++ app/controllers/tobanhyos_controller.rb
@@ -1,3 +1,9 @@
 class TobanhyosController < ApplicationController
+  def index
+    @tobanhyos = Tobanhyo.all.group_by(&:start_of_period).keys
+  end
 
+  def show
+    @tobanhyo = Tobanhyo.where(start_of_period: params[:start_of_period]).group_by(&:fixed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  root 'tobanhyos#index'
+  get 'tobanhyos/show/:start_of_period' => 'tobanhyos#show'
 end
